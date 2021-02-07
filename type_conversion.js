8af1c027f6a9d904ed9e6cfa0bbcd00fd618afbc
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-07 17:59:16 +0800

    Push to github -initial commit

---
+++ type_conversion.js
@@ -0,0 +1,39 @@
+let val;
+
+
+// Number to string
+val = String(12345);
+val = String(5 + 5);
+//Bool to string
+val = String(true);
+//Date to string
+val = String(new Date());
+//Array to string
+val = String([1,2,3,4]);
+
+// toString();
+
+val =  (5).toString();
+val =  true.toString();
+
+// string to Number
+val = Number('5');
+val = Number(true);
+val = Number([1,2,3,4,5]);
+
+
+val = parseInt('24.25');
+val = parseFloat('24.25');
+
+
+const val1 = String(4);
+const val2 = 9;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+
+
+
+
+
+console.log(typeof val);
